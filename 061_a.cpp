0fdb4b582ae80cad12384c2cb0a0d9ab65f4c4b2
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-08-28 02:15:55 +1000

    B - Counting Roads

--- 061_a.cpp
+++ 061_a.cpp
@@ -9,6 +9,7 @@
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0); std::cout.tie(0);
+
     int a = 0, b = 0, c = 0;
     std::cin >> a >> b >> c;
     if (c >= a && c <= b)
