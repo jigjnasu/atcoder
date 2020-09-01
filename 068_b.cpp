99b77e53f2fd82ed73f4002c60a44b5a0ce2b52c
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-02 01:44:39 +1000

    break number

--- 068_b.cpp
+++ 068_b.cpp
@@ -1,18 +1,22 @@
 /*
   Contest 068
-  A - ABCxxx
+  B - Break Number
   Rakesh Kumar --> 01/09/2020
  */
 
 #include <bits/stdc++.h>
 
+/*
+  Maths question -->
+  Max div by 2 number will be log2(n)
+*/
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0); std::cout.tie(0);
-
     int n = 0;
     std::cin >> n;
-    std::cout << "ABC" << n << std::endl;
+    std::cout << (1 << static_cast<int>(std::floor(std::log2(n)))) << std::endl;
 
     return 0;
 }
