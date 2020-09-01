7bf3b5595879c895716e1345bab6a87cadfa328f
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-01 23:56:54 +1000

    066 a

---
+++ 066_a.cpp
@@ -0,0 +1,14 @@
+/*
+  Contest 066
+  A - ringring
+  Rakesh Kumar --> 01/09/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    return 0;
+}
