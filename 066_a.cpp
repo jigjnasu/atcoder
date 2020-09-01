ad7a07e14f6ad834c1ec6e40c54c6616d65ba955
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-02 00:00:03 +1000

    ringing bell

--- 066_a.cpp
+++ 066_a.cpp
@@ -10,5 +10,9 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0); std::cout.tie(0);
 
+    int a = 0, b = 0, c = 0;
+    std::cin >> a >> b >> c;
+    std::cout << std::min(a + b, std::min(b + c, a + c)) << std::endl;
+
     return 0;
 }
