7d7bec07f818208836b82b77f08a54392af72b42
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-03 23:57:52 +1000

    palindromic number

--- 070_a.cpp
+++ 070_a.cpp
@@ -9,6 +9,9 @@
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0); std::cout.tie(0);
+    int n = 0;
+    std::cin >> n;
+    std::cout << (n / 100 == n % 10 ? "Yes" : "No") << std::endl;
 
     return 0;
 }
