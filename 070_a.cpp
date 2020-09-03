40229762e9b240452f29ee501da118bd213f5d64
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-03 23:54:55 +1000

    palindromic number

---
+++ 070_a.cpp
@@ -0,0 +1,14 @@
+/*
+  Contest 070
+  A - Palindromic Number
+  Rakesh Kumar --> 03/09/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    return 0;
+}
