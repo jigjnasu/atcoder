c6b2b600fdf708c77f7efe74455a6d0ec9566209
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-09-28 23:59:27 +1000

    entrance examination

---
+++ abc117_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 116
+  A - Entrance Examination
+  Rakesh Kumar --> 28/09/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int t = 0, x = 0;
+    std::cin >> t >> x;
+    std::cout << t / static_cast<double>(x) << std::endl;
+
+    return 0;
+}
