c11b2bfd3961e44ac55ec8238d1ebabd1e1fa2d7
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-03 21:50:17 +1000

    fifty-fifty

---
+++ abc132_a.cpp
@@ -0,0 +1,28 @@
+/*
+  Contest 133
+  A - Fifty-Fifty
+  Rakesh Kumar --> 03/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    std::string s;
+    std::getline(std::cin, s);
+    std::unordered_map<char, int> dict;
+    for (char c : s)
+        ++dict[c - 'A'];
+    std::string result = "Yes";
+    for (const auto& p : dict) {
+        if (p.second != 2) {
+            result = "No";
+            break;
+        }
+    }
+    std::cout << result << std::endl;
+
+    return 0;
+}
