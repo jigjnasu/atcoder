cb530b2c9747cdaa27f5d60b347ca40d1cc7c5b8
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-03 22:23:11 +1000

    dodecagon

---
+++ abc134_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 134
+  A - Dodecagon
+  Rakesh Kumar --> 03/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int r = 0;
+    std::cin >> r;
+    std::cout << (3 * (r * r)) << std::endl;
+
+    return 0;
+}
