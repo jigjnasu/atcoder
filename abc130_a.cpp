a0200689361ae7adc2d5c26b9aa6919bd5fc931c
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-03 17:12:46 +1000

    rounding

---
+++ abc130_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 130
+
+  Rakesh Kumar --> 03/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int x = 0, a = 0;
+    std::cin >> x >> a;
+    std::cout << (x < a ? 0 : 10) << std::endl;
+
+    return 0;
+}
