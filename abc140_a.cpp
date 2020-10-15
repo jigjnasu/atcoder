90e85ef21c2ce8db8571c8ede118ffdd9a8adc67
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-16 02:41:56 +1100

    password

---
+++ abc140_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 140
+  A - Password
+  Rakesh Kumar --> 16/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0;
+    std::cin >> n;
+    std::cout << std::pow(n, 3) << std::endl;
+
+    return 0;
+}
