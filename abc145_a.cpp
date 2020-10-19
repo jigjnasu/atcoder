868b4e61b59f5219ee4ecdf615befc066da876eb
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-19 13:08:53 +1100

    circle

---
+++ abc145_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 145
+  A - Circle
+  Rakesh Kumar --> 19/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int r = 0;
+    std::cin >> r;
+    std::cout << r * r << std::endl;
+
+    return 0;
+}
