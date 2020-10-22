40f78ecede00cdd323f09d4d84cb575d5ead7a3a
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-22 11:18:18 +1100

    round one

---
+++ abc148_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 148
+  A - Round One
+  Rakesh Kumar --> 22/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int a = 0, b = 0;
+    std::cin >> a >> b;
+    std::cout << 6 - (a + b) << std::endl;
+
+    return 0;
+}
