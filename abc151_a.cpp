4f397e6ac50165d7780f4223265ea7bc4d7fb18a
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-25 11:37:41 +1100

    next alphabet

---
+++ abc151_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 151
+  A - Next Alphabet
+  Rakesh Kumar --> 25/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    char c;
+    std::cin >> c;
+    std::cout << static_cast<char>(c + 1) << std::endl;
+
+    return 0;
+}
