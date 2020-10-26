a82c6078a13bd3c1413e49f1637e1e5cc4fb83a9
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-10-26 16:08:50 +1100

    ac or wa

---
+++ abc152_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 152
+  A - AC or WA
+  Rakesh Kumar --> 26/10/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0, m = 0;
+    std::cin >> n >> m;
+    std::cout << (m >= n ? "Yes" : "No") << std::endl;
+
+    return 0;
+}
