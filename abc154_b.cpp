41c97768fbb66443b5689edaa0f165ffd02762b7
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-11-05 04:27:23 +1100

    i miss you

---
+++ abc154_b.cpp
@@ -0,0 +1,19 @@
+/*
+  Contest 154
+  B - I miss you...
+  Rakesh Kumar --> 05/11/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    std::string s;
+    std::cin >> s;
+    std::string x(s.size(), 'x');
+    std::cout << x << std::endl;
+
+    return 0;
+}
