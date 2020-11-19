c4bd8d5fbea9e77341f3a617bb4a99be931bb76f
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-11-20 02:40:15 +1100

    circle pond

---
+++ abc163_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 163
+  A - Circle Pond
+  Rakesh Kumar --> 20/11/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int r = 0;
+    std::cin >> r;
+    std::cout << 2 * M_PI * r << std::endl;
+
+    return 0;
+}
