c363d4b9dfa2c335ab21339f0187788286a953b0
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-11-23 02:16:02 +1100

    166 A

---
+++ abc166_a.cpp
@@ -0,0 +1,22 @@
+/*
+  Contest 165
+  A - We Love Golf
+  Rakesh Kumar --> 23/11/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    std::string s;
+    std::cin >> s;
+    std::string r = "ABC";
+    if (s == "ABC")
+        r = "ARC";
+    std::cout << r << std::endl;
+
+
+    return 0;
+}
