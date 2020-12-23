59c464a8dc9f9a87c19dd1730bff861d9fd12423
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-12-23 13:59:20 +1100

    air conditioner

---
+++ abc174_a.cpp
@@ -0,0 +1,18 @@
+/*
+  Contest 174
+  A - Air Conditioner
+  Rakesh Kumar --> 23/12/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0;
+    std::cin >> n;
+    std::cout << (n >= 30 ? "Yes" : "No") << std::endl;
+
+    return 0;
+}
