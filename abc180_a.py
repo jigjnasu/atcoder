ae959c5049fbc39ee2ff1eed5cae5f1d80a9da98
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-12-30 00:40:56 +1100

    box

---
+++ abc180_a.py
@@ -0,0 +1,12 @@
+'''
+  Contest 180
+  A - box
+  Rakesh Kumar --> 30/12/2020
+'''
+
+def solve():
+    n, a, b = list(map(int, input().split()))
+    print(n - a + b)
+
+if __name__ == '__main__':
+    solve()
