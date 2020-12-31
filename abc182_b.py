ec8968a5ef78b846b077e0dd29b7fbd914d90e0d
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-01 01:21:36 +1100

    twiblr

---
+++ abc182_b.py
@@ -0,0 +1,27 @@
+'''
+  Contest 182
+  A - twiblr
+  Rakesh Kumar --> 01/01/2021
+'''
+
+def divisors(n, arr):
+    c = 0
+    for e in arr:
+        if e % n == 0:
+            c += 1
+    return c
+
+def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    m = 0
+    v = 0
+    for i in range(2, max(arr) + 1):
+        c = divisors(i, arr)
+        if c > m:
+            m = c
+            v = i
+    print(v)
+
+if __name__ == '__main__':
+    solve()
