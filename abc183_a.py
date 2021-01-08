3f8d94d903a3a179622644b1898ad5d607b71ffd
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-09 10:50:14 +1100

    reLu

---
+++ abc183_a.py
@@ -0,0 +1,14 @@
+'''
+  Contest 183
+  A - ReLU
+  Rakesh Kumar --> 09/01/2021
+'''
+
+def solve():
+    n = int(input())
+    if n < 0:
+        n = 0
+    print(n)
+
+if __name__ == '__main__':
+    solve()
