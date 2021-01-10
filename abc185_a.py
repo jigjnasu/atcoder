331ee1965ec4b032ea69be24a22d613ce8111db0
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-10 11:53:28 +1100

    abc preparation

---
+++ abc185_a.py
@@ -0,0 +1,11 @@
+'''
+  Contest 185
+  A - ABC Preparation
+  Rakesh Kumar --> 10/01/2021
+'''
+
+def solve():
+    print(min(list(map(int, input().split()))))
+
+if __name__ == '__main__':
+    solve()
