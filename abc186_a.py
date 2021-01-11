5e48e1076348658a754734303bd7a27a752766ac
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-11 23:41:10 +1100

    brick

---
+++ abc186_a.py
@@ -0,0 +1,12 @@
+'''
+  Contest 186
+  A - Brick
+  Rakesh Kumar --> 11/01/2021
+'''
+
+def solve():
+    n, w = map(int, input().split())
+    print(n // w)
+
+if __name__ == '__main__':
+    solve()
