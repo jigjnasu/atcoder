1840bb52171fafc6e69aa516a8dd99d86fee8f6e
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-01-15 09:13:10 +1100

    three positive shot

---
+++ agc002_a.py
@@ -0,0 +1,22 @@
+'''
+  Contest 188
+  A - Three-Point Shot
+  Rakesh Kumar --> 15/01/2021
+'''
+
+def solve():
+    a, b = map(int, input().split())
+    if a > 0 and b > 0:
+        print('Positive')
+    elif a < 0 and b < 0:
+        c = abs(b) - abs(a) + 1
+        if c & 1 == 1:
+            print('Negative')
+        else:
+            print('Positive')
+    else:
+        print('Zero')
+
+
+if __name__ == '__main__':
+    solve()
