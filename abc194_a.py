be0ca5452c190d4b2a7a35435e3367d72c898fc5
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-03-19 09:15:42 +1100

    I scream

---
+++ abc194_a.py
@@ -0,0 +1,20 @@
+'''
+  Contest 194
+  A - I Scream
+  Rakesh Kumar --> 19/03/2021
+'''
+
+def solve():
+    a, b = map(int, input().split())
+    c = a + b
+    if c >= 15 and b >= 8:
+        print(1)
+    elif c >= 10 and b >= 3:
+        print(2)
+    elif c >= 3:
+        print(3)
+    else:
+        print(4)
+
+if __name__ == '__main__':
+    solve()
