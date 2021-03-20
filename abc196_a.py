47fcea47819b326fc023d69ed113a74baf3ea57e
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-03-21 02:33:26 +1100

    196 a

---
+++ abc196_a.py
@@ -0,0 +1,13 @@
+'''
+  Contest 196
+  A - Difference Max
+  Rakesh Kumar --> 21/03/2021
+'''
+
+def solve():
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+    print(b - c)
+
+if __name__ == '__main__':
+    solve()
