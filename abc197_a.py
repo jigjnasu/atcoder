e453d3fa34a1225fc7e68988f7ceb4bb0adf30a3
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-04-13 23:53:06 +1000

    rotate

---
+++ abc197_a.py
@@ -0,0 +1,12 @@
+'''
+  Contest 197
+  A - Rotate
+  Rakesh Kumar --> 13/04/2021
+'''
+
+def solve():
+    s = str(input())
+    print('{}{}{}'.format(s[1], s[2], s[0]))
+
+if __name__ == '__main__':
+    solve()
