7b3d56e1b62bb51a2fe23414a4a549d09dd7561b
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-05-31 11:34:54 +1000

    ABC 200 A

---
+++ abc200_a.py
@@ -0,0 +1,14 @@
+'''
+  Contest 200
+  A - Century
+  Rakesh Kumar --> 31/05/2021
+'''
+
+import math
+
+def solve():
+    y = int(input())
+    print(math.ceil(y / 100))
+
+if __name__ == '__main__':
+    solve()
