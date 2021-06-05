bf9b68a9192f4373e2c38acae375a1ba6eecb9e7
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-06-05 14:35:38 +1000

    201

---
+++ abc201_a.py
@@ -0,0 +1,16 @@
+'''
+  Contest 201
+  A - Tiny Arithmetic Sequence
+  Rakesh Kumar --> 31/05/2021
+'''
+
+def solve():
+    a = list(map(int, input().split()))
+    a.sort()
+    if a[1]-a[0] == a[2]-a[1]:
+        print('Yes')
+    else:
+        print('No')
+
+if __name__ == '__main__':
+    solve()
