f91ccdaf12f29b37b82c69f7f90cc17a8cfbdff9
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-06-15 16:58:32 +1000

    204 a

---
+++ abc204_b.cpp
@@ -0,0 +1,14 @@
+/*
+ * Contest 204
+ * B - Nuts
+ * Rakesh Kumar --> 15/06/2021
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    int n = 0;
+    std::cin >> n;
+    return 0;
+}
