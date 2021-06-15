31640d9aadccdff80c015dca7890426eb0b5e920
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-06-15 17:00:15 +1000

    abc 204 b

--- abc204_b.cpp
+++ abc204_b.cpp
@@ -8,7 +8,15 @@
 
 int main() {
     std::ios::sync_with_stdio(false);
-    int n = 0;
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0, v = 0;
     std::cin >> n;
+    int r = 0;
+    while (n--) {
+        std::cin >> v;
+        r += std::max(0, v-10);
+    }
+    std::cout << r << std::endl;
     return 0;
 }
