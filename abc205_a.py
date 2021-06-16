7765a8e1566bca208b598919b99afc236a416a95
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-06-16 17:23:07 +1000

    abc 205 a

---
+++ abc205_a.py
@@ -0,0 +1,12 @@
+'''
+  Contest 205
+  A - kcal
+  Rakesh Kumar --> 16/06/2021
+'''
+
+def solve():
+    a, b = map(int, input().split())
+    print (0.01 * a * b)
+
+if __name__ == '__main__':
+    solve()
