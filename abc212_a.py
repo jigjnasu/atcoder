b03c63f3c102449205e8aa5592dc7406cf9aabcb
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-10-21 12:52:53 +1100

    abc 211 a

---
+++ abc212_a.py
@@ -0,0 +1,17 @@
+'''
+  Contest 211
+  A - Alloy
+  Rakesh Kumar --> 21/10/2021
+'''
+
+def solve():
+    arr = [int(x) for x in input().split()]
+    if arr[0] == 0:
+        print('Silver')
+    elif arr[1] == 0:
+        print('Gold')
+    else:
+        print('Alloy')
+
+if __name__ == '__main__':
+    solve()
