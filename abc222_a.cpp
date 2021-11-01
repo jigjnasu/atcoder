d839fd20d356dc852355e92fca58ad81ef183b73
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-11-01 22:12:02 +1100

    abc222 a

---
+++ abc222_a.cpp
@@ -0,0 +1,15 @@
+/*
+  Contest 222
+  A - Four Digits
+  Rakesh Kumar --> 01/11/2021
+*/
+
+#include <bits/stdc++.h>
+
+int main() {
+    int v = 0;
+    scanf("%d", &v);
+    printf("%04d\n", v);
+
+    return 0;
+}
