ca9792893e859438e16602e03b910763d0a60f7f
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-11-04 11:31:59 +1100

    abc 224 a

---
+++ abc224_a.py
@@ -0,0 +1,15 @@
+'''
+  Contest 224
+  A - Tires
+  Rakesh Kumar --> 04/11/2021
+'''
+
+def solve():
+    s = str(input())
+    if s[-2:] == 'er':
+        print('er')
+    else:
+        print('ist')
+
+if __name__ == '__main__':
+    solve()
