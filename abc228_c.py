6bbcccd05d3808fa31499c2c5f89f360716f143b
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-12-19 02:10:48 +1100

    SOLUTION 228 c

--- abc228_c.py
+++ abc228_c.py
@@ -6,17 +6,20 @@
 
 def solve():
     n, k = map(int, input().split())
-    m = 0
-    a = []
+    total_marks = []
+    student_marks = []
+    topper = 0
     for _ in range(n):
-        s = sum([int(x) for x in input().split()])
-        a.append(s)
-        m = max(s, m)
-    for s in a:
-        if abs(s - m) <= 300:
+        marks = sum([int(x) for x in input().split()])
+        total_marks.append(marks)
+        student_marks.append(marks)
+    total_marks.sort(reverse=True)
+    topper = total_marks[k - 1]
+    for s in student_marks:
+        if s + 300 >= topper:
             print('Yes')
         else:
-            print('No')    
+            print('No')
 
 if __name__ == '__main__':
     solve()
