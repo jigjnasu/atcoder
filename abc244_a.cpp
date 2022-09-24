095ecc8cc51389d49b54e91103f7f71ab050e482
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-09-25 02:55:48 +1000

    abc 244 a

---
+++ abc244_a.cpp
@@ -0,0 +1,21 @@
+/*
+    Contest 244
+    A - Last Letter
+    Rakesh Kumar
+*/
+    
+#include <bits/stdc++.h>
+
+using ull = unsigned long long int;
+    
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int n = 0;
+    std::string s;
+    std::cin >> n >> s;
+    std::cout << s[n - 1] << std::endl;
+
+    return 0;
+}
