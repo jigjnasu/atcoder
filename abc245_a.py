14f5ac86f3f21cea4992b14d0a2035e22b66f0e6
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-09-27 00:21:31 +1000

    abc 245 a

---
+++ abc245_a.py
@@ -0,0 +1,20 @@
+'''
+  Contest 245
+  A - Good morning
+  Rakesh Kumar
+'''
+
+import datetime
+from xmlrpc.client import DateTime
+
+def solve():
+    a, b, c, d = map(int, input().split())
+    t = datetime.time(a, b, 0)
+    a = datetime.time(c, d, 1)
+    if t < a:
+        print('Takahashi')
+    else:
+        print('Aoki')
+
+if __name__ == '__main__':
+    solve()
