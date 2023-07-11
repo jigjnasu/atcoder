0953e86a514168246f46099b3d89e51847c9fc90
Rakesh Kumar <cpp.rakesh@gmail.com>
2023-07-12 05:23:36 +1000

    abc 261 a

---
+++ abc261_a.py
@@ -0,0 +1,12 @@
+'''
+  Contest 258
+  A - Intersection
+  Rakesh Kumar
+'''
+
+def solve():
+    arr = list(map(int, input().split()))
+    print(max(min(arr[1], arr[3]) - max(arr[0], arr[2]), 0))
+    
+if __name__ == '__main__':
+    solve()
