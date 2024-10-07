fa31ea6ea2eea67865c42180c0b77cd0a7f7f98e
Rakesh Kumar <cpp.rakesh@gmail.com>
2024-10-07 22:44:40 +1100

    abc 267 a

---
+++ abc267_a.py
@@ -0,0 +1,12 @@
+'''
+  Contest 267
+  A - Saturday
+  Rakesh Kumar
+'''
+
+def solve():
+    days_left = {'Monday' : 5, 'Tuesday' : 4, 'Wednesday' : 3, 'Thursday' : 2, 'Friday' : 1}
+    print(days_left[input()])
+
+if __name__ == '__main__':
+    solve()
